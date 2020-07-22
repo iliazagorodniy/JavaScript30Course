a45e84cfa3b3784bb0177fd75817530ec68d4695
Ilia <izagorodn@gmail.com>
2020-07-22 17:35:34 +0300

    new clock styling

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,11 +1,22 @@
 const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
 const setDate = () => {
     const now = new Date();
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+    let diff = Math.round((now - today) / 1000);
 
     const seconds = now.getSeconds();
-    const secondsDeg = ((seconds / 60) * 360) + 90;
+    const secondsDeg = diff * 6 + 90;
     secondHand.style.transform = `rotate(${secondsDeg}deg)`;
-    console.log(seconds);
-}
 
+    const minutes = now.getMinutes();
+    const minutesDeg = (minutes * 6) + 90;
+    minuteHand.style.transform = `rotate(${minutesDeg}deg)`;
+
+    const hours = now.getHours();
+    const hoursDeg = (hours * 30) + 90;
+    hourHand.style.transform = `rotate(${hoursDeg}deg)`;
+}
 setInterval(setDate, 1000);
